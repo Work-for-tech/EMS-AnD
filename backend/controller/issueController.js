4835d88ee2ae39644a63f68d99f5b98a43fb2b07
Modiperin <modiperin@gmail.com>
2023-10-17 14:54:39 +0530

    issue added

---
+++ backend/controller/issueController.js
@@ -0,0 +1,55 @@
+const issueSchema = require('../models/issueSchema')
+const storeSchema = require('../models/storeSchema')
+
+
+exports.addIssue = async (req, res) => {
+    try {
+        const issueSlip = await issueSchema.create(req.body)
+        if (issueSchema != null && req.body.quantity>=0) {
+            await store
+                .findOneAndUpdate(
+                    {
+                        $and: [
+                            { desc: req.body.desc },
+                            { catalog_number: req.body.catalog_number },
+                            { rating_value: req.body.rating_value },
+                            { companyId: req.body.companyId },
+                        ],
+                    },
+                    { $inc: { quantity: -(req.body.quantity) } },
+                )
+                .exec();
+
+            res.status(201).json({ message: "Success", data: issueSlip });
+        } else {
+            throw new Error("Something went wrong in issueSlip")
+        }
+
+    } catch (e) {
+        res.status(500).json({
+            message: "Something went wrong in issueSlip",
+            error: e
+        })
+    }
+}
+
+exports.getIssueList = async (req, res) => {
+
+    try {
+        const issueList = await issueSchema.find({})
+        if (issueList != null) {
+            res.status(200).json({
+                message: "Issue List retrieved successfully",
+                data: issueList
+            })
+        } else {
+            throw new Error("Something went wrong in IssueList")
+        }
+    } catch (e) {
+        res.status(500).json({
+            message: "Something went wrong in issueSlip",
+            error: e
+        })
+    }
+}
+
